536ff1523fdd5116a90b7b37dd65314a860723cb
Aditya <aadi099012@gmail.com>
2024-09-26 14:49:43 +0530

    ADDING REACT SLICE

---
+++ Blog-app/src/store/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const intialState ={
+    status:false,
+    userData:null,
+}
+const authSlice = createSlice({
+    name:"auth",
+    initialState: intialState,
+    reducers:{
+        login:(state, action)=>{
+            state.status = true;
+            state.userData = action.payload.userData;
+
+        },
+        logOut:(state)=>{
+           state.status = false;
+           state.userData = null;
+        }
+    }
+
+})
+
+ export {logOut,login}  from authSlice.actions;
+
+export default  authSlice.reducer;
