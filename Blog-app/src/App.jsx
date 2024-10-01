4cb12d063f6021533d657800595f8cd305bdaae8
Aditya <aadi099012@gmail.com>
2024-10-01 08:48:16 +0530

    debugg code

--- Blog-app/src/App.jsx
+++ Blog-app/src/App.jsx
@@ -1,17 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
-import PostForm from './components/post-form/PostForm'
-import conf from './conf/conf'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-
-  return (
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
   
-    <>
-      <h1 className='bg-red-400'> React With appWrite</h1>
-      <PostForm/>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
--- Blog-app/src/store/authSlice.js
+++ Blog-app/src/store/authSlice.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const intialState ={
-    status:false,
-    userData:null,
+const initialState = {
+    status : false,
+    userData: null
 }
+
 const authSlice = createSlice({
-    name:"auth",
-    initialState: intialState,
-    reducers:{
-        login:(state, action)=>{
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
             state.status = true;
             state.userData = action.payload.userData;
-
         },
-        logOut:(state)=>{
-           state.status = false;
-           state.userData = null;
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
         }
-    }
-
+     }
 })
 
-export const {login, logOut} = authSlice.actions;
+export const {login, logout} = authSlice.actions;
 
-export default  authSlice.reducer;
+export default authSlice.reducer;
